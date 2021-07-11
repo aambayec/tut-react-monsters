c70adc2e89c0bae88e690862cbd1e1f74b5c4a62
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-12 00:58:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+class App extends Component {
+  constructor() {
+    // super helps us with 'this' by calling React.Component's constructor( )
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    // fetch is a native js function to call api
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
