674457186fc42a8f26cde977d48c75e45c086c6a
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-12 01:57:06 +0800

    page styling

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
